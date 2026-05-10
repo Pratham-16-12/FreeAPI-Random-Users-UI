c90e19b7d7188d1a5d3ce8084156d7d9246614fa
Pratham <parthambhardwaj1@gmail.com>
2026-05-11 00:13:38 +0530

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this line. IMPORTANT: It must match your exact GitHub repository name!
+  base: '/FreeAPI-Random-Users-UI/',
 })
